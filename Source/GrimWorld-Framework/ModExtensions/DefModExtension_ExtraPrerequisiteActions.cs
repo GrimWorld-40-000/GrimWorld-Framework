bd46301a26016ed4094f5cd70a846a047342ac74
LazyEpsilon <53150458+LazyEpsilon@users.noreply.github.com>
2023-10-23 18:59:02 +0100

    Initial framework commit including basic research system

---
+++ Source/GrimWorld-Framework/ModExtensions/DefModExtension_ExtraPrerequisiteActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace GW_Frame
+{
+    public class DefModExtension_ExtraPrerequisiteActions : DefModExtension, IExposable
+    {
+        public ThingDef StudyLocation;
+        public List<StudyRequirement> ItemStudyRequirements;
+
+        public void ExposeData()
+        {
+            Scribe_Defs.Look(ref StudyLocation, "StudyLocation");
+            Scribe_Collections.Look(ref ItemStudyRequirements, "StudyRequirements", LookMode.Deep);
+        }
+    }
+}
---
+++ Source/GrimWorld-Framework/Statics/GrimWorld_FrameworkMod.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace GW_Frame
+{
+    public class GrimWorld_FrameworkMod : Mod
+    {
+        private const string ModName = "GrimWorld_Framework.Mod";
+
+        public GrimWorld_FrameworkMod(ModContentPack content) : base(content)
+        {
+            new Harmony(ModName).PatchAll();
+        }
+    }
+}
