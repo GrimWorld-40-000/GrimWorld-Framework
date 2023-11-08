bd40f211fb302a1e2cb8f4935d8c5fbe0d759d5c
Maxim <MaximSw@web.de>
2023-11-08 17:15:20 +0100

    coloring fixes

--- Source/ItemPainter/GW4KArmor/Graphic_TriColorMask.cs
+++ Source/ItemPainter/GW4KArmor/Graphic_TriColorMask.cs
@@ -9,28 +9,28 @@ public class Graphic_TriColorMask : Graphic_Multi
 	public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
 	{
 		var apparel = thing as Apparel;
-		var flag = apparel != null;
-		if (flag) rot = Rot4.South;
+		if (apparel != null) 
+			rot = Rot4.South;
 
 		base.DrawWorker(loc, rot, thingDef, thing, extraRotation);
 	}
 
 	public override void Print(SectionLayer layer, Thing thing, float extraRotation)
 	{
-		var flag = false;
+		var rotateBack = false;
 		var rotation = thing.Rotation;
+		
 		var apparel = thing as Apparel;
-		var flag2 = apparel != null;
-		if (flag2)
+		if (apparel != null)
 		{
 			rotation = thing.Rotation;
 			thing.Rotation = Rot4.South;
-			flag = true;
+			rotateBack = true;
 		}
 
 		base.Print(layer, thing, extraRotation);
-		var flag3 = flag;
-		if (flag3) thing.Rotation = rotation;
+		if (rotateBack)
+			thing.Rotation = rotation;
 	}
 
 	public override void Init(GraphicRequest req)
@@ -46,55 +46,45 @@ public class Graphic_TriColorMask : Graphic_Multi
 		array[1] = ContentFinder<Texture2D>.Get(req.path + "_east", false);
 		array[2] = ContentFinder<Texture2D>.Get(req.path + "_south", false);
 		array[3] = ContentFinder<Texture2D>.Get(req.path + "_west", false);
-		var flag = array[0] == null;
-		if (flag)
+		
+		if (array[0] == null)
 		{
-			var flag2 = array[2] != null;
-			if (flag2)
+			if (array[2] != null)
 			{
 				array[0] = array[2];
 				drawRotatedExtraAngleOffset = 180f;
 			}
+			else if (array[1] != null)
+			{
+				array[0] = array[1];
+				drawRotatedExtraAngleOffset = -90f;
+			}
+			else if (array[3] != null)
+			{
+				array[0] = array[3];
+				drawRotatedExtraAngleOffset = 90f;
+			}
 			else
 			{
-				var flag3 = array[1] != null;
-				if (flag3)
-				{
-					array[0] = array[1];
-					drawRotatedExtraAngleOffset = -90f;
-				}
-				else
-				{
-					var flag4 = array[3] != null;
-					if (flag4)
-					{
-						array[0] = array[3];
-						drawRotatedExtraAngleOffset = 90f;
-					}
-					else
-					{
-						array[0] = ContentFinder<Texture2D>.Get(req.path, false);
-					}
-				}
+				array[0] = ContentFinder<Texture2D>.Get(req.path, false);
 			}
 		}
 
-		var flag5 = array[0] == null;
-		if (flag5)
+		if (array[0] == null)
 		{
 			Log.Error("Failed to find any textures at " + req.path + " while constructing " +
 			          this.ToStringSafe());
 		}
 		else
 		{
-			var flag6 = array[2] == null;
-			if (flag6) array[2] = array[0];
-
-			var flag7 = array[1] == null;
-			if (flag7)
+			if (array[2] == null)
 			{
-				var flag8 = array[3] != null;
-				if (flag8)
+				array[2] = array[0];
+			}
+
+			if (array[1] == null)
+			{
+				if (array[3] != null)
 				{
 					array[1] = array[3];
 					eastFlipped = DataAllowsFlip;
@@ -105,11 +95,9 @@ public class Graphic_TriColorMask : Graphic_Multi
 				}
 			}
 
-			var flag9 = array[3] == null;
-			if (flag9)
+			if (array[3] == null)
 			{
-				var flag10 = array[1] != null;
-				if (flag10)
+				if (array[1] != null)
 				{
 					array[3] = array[1];
 					westFlipped = DataAllowsFlip;
@@ -126,50 +114,50 @@ public class Graphic_TriColorMask : Graphic_Multi
 			array2[1] = ContentFinder<Texture2D>.Get(maskPath + "_east", false);
 			array2[2] = ContentFinder<Texture2D>.Get(maskPath + "_south", false);
 			array2[3] = ContentFinder<Texture2D>.Get(maskPath + "_west", false);
-			var flag11 = array2[0] == null;
-			if (flag11)
+			
+			if (array2[0] == null)
 			{
-				var flag12 = array2[2] != null;
-				if (flag12)
+				if (array2[2] != null)
 				{
 					array2[0] = array2[2];
 				}
-				else
+				else if (array2[1] != null)
 				{
-					var flag13 = array2[1] != null;
-					if (flag13)
-					{
-						array2[0] = array2[1];
-					}
-					else
-					{
-						var flag14 = array2[3] != null;
-						if (flag14) array2[0] = array2[3];
-					}
+					array2[0] = array2[1];
+				}
+				else if (array2[3] != null)
+				{
+					array2[0] = array2[3];
 				}
 			}
 
-			var flag15 = array2[2] == null;
-			if (flag15) array2[2] = array2[0];
-
-			var flag16 = array2[1] == null;
-			if (flag16)
+			if (array2[2] == null)
 			{
-				var flag17 = array2[3] != null;
-				if (flag17)
-					array2[1] = array2[3];
-				else
-					array2[1] = array2[0];
+				array2[2] = array2[0];
 			}
 
-			var flag18 = array2[3] == null;
-			if (flag18)
+			if (array2[1] == null)
 			{
-				var flag19 = array2[1] != null;
-				if (flag19)
-					array2[3] = array2[1];
+				if (array2[3] != null)
+				{
+					array2[1] = array2[3];
+				}
 				else
+				{
+					array2[1] = array2[0];
+				}
+			}
+
+			if (array2[3] == null)
+			{
+				if (array2[1] != null)
+				{
+					array2[3] = array2[1];
+				}
+				else
+				{
 					array2[3] = array2[0];
+				}
 			}
 
 			for (var i = 0; i < mats.Length; i++)
