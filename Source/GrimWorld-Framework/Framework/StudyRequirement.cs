69589348a9bba6fb92a253382195310a4f6b88b4
Robert <128163141+KonradHeinser@users.noreply.github.com>
2023-11-18 12:33:52 -0500

    Add files via upload

---
+++ Source/GrimWorld-Framework/Framework/StudyRequirement.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace GW_Frame
+{
+    public class StudyRequirement : IExposable
+    {
+        public ThingDef StudyObject;
+        public int NumberRequired;
+
+        public void ExposeData()
+        {
+            Scribe_Defs.Look(ref StudyObject, "StudyObject");
+            Scribe_Values.Look(ref NumberRequired, "NumberRequired");
+        }
+    }
+}
---
+++ Source/GrimWorld-Framework/Patches/ResearchProjectDef_get_PrerequisitesCompleted_Patch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace GW_Frame
+{
+    [HarmonyPatch(typeof(ResearchProjectDef), "get_PrerequisitesCompleted")]
+    public static class ResearchProjectDef_get_PrerequisitesCompleted_Patch
+    {
+        public static bool Postfix(bool __result, ResearchProjectDef __instance)
+        {
+            if (__result && __instance.HasModExtension<DefModExtension_ExtraPrerequisiteActions>())
+                return __result && Find.World.GetComponent<WorldComponent_StudyManager>().CompletedAllRequirements(__instance);
+            return __result;
+        }
+    }
+}
