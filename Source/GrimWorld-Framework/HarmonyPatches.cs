9c36a3b705f858e1c39509d72e2c44c96a83a455
DimonSever000 <DimonSever000@gmail.com>
2024-05-22 20:35:49 +0300

    Weapon tag settings system
    1.0

--- Source/GrimWorld-Framework/HarmonyPatches.cs
+++ Source/GrimWorld-Framework/HarmonyPatches.cs
@@ -6,6 +6,7 @@ using HarmonyLib;
 using RimWorld;
 using UnityEngine;
 using Verse;
+using GW_Frame.Settings;
 
 namespace GW_Frame
 {
@@ -37,8 +38,16 @@ namespace GW_Frame
                 postfix: new HarmonyMethod(patchType, nameof(DropShieldIfEquippedTwoHandedPostFix)));
             harmony.Patch(AccessTools.Method(typeof(Pawn_ApparelTracker), "Wear"),
                 postfix: new HarmonyMethod(patchType, nameof(DropTwoHandedIfEquippedShieldPostFix)));
+            harmony.Patch(AccessTools.Method(typeof(Log), "ResetMessageCount"),
+                postfix: new HarmonyMethod(patchType, nameof(ResetMessageCountPostfix)));
         }
 
+        public static void ResetMessageCountPostfix()
+        {
+            Settings.Settings.Instance.CastChanges();
+            ThingCategoryDef.Named("GW_Shield").ResolveReferences();
+            ThingCategoryDef.Named("GW_TwoHanded").ResolveReferences();
+        }
         public static void CanEquipPostfix(ref bool __result, Thing thing, Pawn pawn, ref string cantReason)
         {
             EquipRestrictExtension extension = thing.def.GetModExtension<EquipRestrictExtension>();
@@ -337,7 +346,7 @@ namespace GW_Frame
 
         public static void DropShieldIfEquippedTwoHandedPostFix(Pawn_EquipmentTracker __instance, ThingWithComps eq)
         {
-            if (!eq.HasThingCategory(DefDatabase<ThingCategoryDef>.GetNamed("TwoHanded"))) return;
+            if (!eq.HasThingCategory(DefDatabase<ThingCategoryDef>.GetNamed("GW_TwoHanded"))) return;
 
             var pawnApparelTracker = __instance.pawn.apparel;
             var allWornApparels = pawnApparelTracker.WornApparel;
@@ -358,7 +367,7 @@ namespace GW_Frame
 
             var pawnPrimaryWeapon = __instance.pawn.equipment.Primary;
 
-            if(pawnPrimaryWeapon != null && pawnPrimaryWeapon.HasThingCategory(DefDatabase<ThingCategoryDef>.GetNamed("TwoHanded"))) {
+            if(pawnPrimaryWeapon != null && pawnPrimaryWeapon.HasThingCategory(DefDatabase<ThingCategoryDef>.GetNamed("GW_TwoHanded"))) {
                 __instance.pawn.equipment.TryDropEquipment(pawnPrimaryWeapon, out var resultEq, __instance.pawn.Position, false);
             }
         }
---
+++ Source/GrimWorld-Framework/Settings/SettingsRecord.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace GW_Frame.Settings
+{
+    public abstract class SettingsRecord : IExposable
+    {
+        public abstract void CastChanges();
+        public abstract void Reset();
+        public abstract void ExposeData();
+    }
+}
