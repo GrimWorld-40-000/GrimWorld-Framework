7fb262bcaecd38985b7bfbefec20d37eee58bc95
DimonSever000 <DimonSever000@gmail.com>
2024-05-22 21:12:06 +0300

    Weapon Tags System

---
+++ Source/GrimWorld-Framework/Settings/SettingsTabRecord.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace GW_Frame.Settings
+{
+    public class SettingsTabRecord : TabRecord
+    {
+        public readonly SettingsTabDef def;
+
+        public SettingsTabRecord(SettingsTabDef def, string label, Action clickedAction, Func<bool> selected)
+            : base(label, clickedAction, selected)
+        {
+            this.def = def;
+        }
+        public virtual void OnGUI(Rect rect)
+        {
+
+        }
+    }
+}
