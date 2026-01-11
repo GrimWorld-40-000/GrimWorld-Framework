e130496b4d8836f011581bec3f48a73f2b71f81f
doop0707 <alma.hue75@gmail.com>
2026-01-11 23:19:40 +0700

    disabled settings

--- Source/GrimWorld-Framework/Settings/SettingsTabDef.cs
+++ Source/GrimWorld-Framework/Settings/SettingsTabDef.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Verse;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Verse;
 
-namespace GW_Frame.Settings
-{
-    public class SettingsTabDef : Def
-    {
-        public Type settingsTabClass;
-        public Type settingsRecordClass;
-    }
-}
+//namespace GW_Frame.Settings
+//{
+//    public class SettingsTabDef : Def
+//    {
+//        public Type settingsTabClass;
+//        public Type settingsRecordClass;
+//    }
+//}
--- Source/GrimWorld-Framework/Settings/SettingsTabRecord.cs
+++ Source/GrimWorld-Framework/Settings/SettingsTabRecord.cs
@@ -1,26 +1,26 @@
-﻿using RimWorld;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
-using Verse;
+﻿//using RimWorld;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UnityEngine;
+//using Verse;
 
-namespace GW_Frame.Settings
-{
-    public class SettingsTabRecord : TabRecord
-    {
-        public readonly SettingsTabDef def;
+//namespace GW_Frame.Settings
+//{
+//    public class SettingsTabRecord : TabRecord
+//    {
+//        public readonly SettingsTabDef def;
 
-        public SettingsTabRecord(SettingsTabDef def, string label, Action clickedAction, Func<bool> selected)
-            : base(label, clickedAction, selected)
-        {
-            this.def = def;
-        }
-        public virtual void OnGUI(Rect rect)
-        {
+//        public SettingsTabRecord(SettingsTabDef def, string label, Action clickedAction, Func<bool> selected)
+//            : base(label, clickedAction, selected)
+//        {
+//            this.def = def;
+//        }
+//        public virtual void OnGUI(Rect rect)
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
